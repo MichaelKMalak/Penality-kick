7ce339e8c80727f4317ff7ade20576b4df7a020e
Michael Khalil Malak <michael.k.malak@gmail.com>
2016-12-06 21:48:33 +0200

    Update main.asm

--- main.asm
+++ main.asm
@@ -18,7 +18,7 @@
  p1_score db 0  ;Intially player 1 score is 0
  p2_score db 0  ;Intially player 2 score is 0
  
- GoalDim db 71, 3, 74, 11 ;X1,Y1, X2,Y2
+ GoalDim db 71, 3, 75, 11 ;X1,Y1, X2,Y2
  current_player db 1                        
  
  Hr_Line_Color db 0c0h
@@ -30,7 +30,24 @@
  Coordinate_BallCurve dw  0205h , 6     
 
  temp dw ? 
- Shoot_Key  db  1Ch; Enter scan code
+ Shoot_Key  db  1Ch; Enter scan code   
+ 
+ Total_Shoots db 1       ;inc until 10 (5 for each player)
+
+ p1_win db 'Player 1 Wins','$'
+ p2_win db 'Player 2 Wins','$'
+ Draw   db 'It is A Draw','$'  
+ 
+ Turn1  db 'Player 1 Turn','$'
+ Turn2  db 'Player 2 Turn','$'
+ Exiting   db 'Exiting The Game','$'  
+ 
+ Goal1 db 'Player 1 Scored A Goal','$'
+ Goal2 db 'Player 2 Scored A Goal','$'
+ Save db 'The Keeper Saved The Ball','$'
+ Outside db 'The Ball is Out !','$'
+
+ 
  
 .CODE   
 MAIN    PROC   
@@ -42,7 +59,7 @@ MAIN    PROC
          
     CALL GetNames
     
-    mov current_player, 2 ;Set player to player 2
+    ;mov current_player, 2 ;Set player to player 2
         
     CALL DrawInterface   
     
@@ -61,12 +78,62 @@ MAIN    PROC
    Print RBCenterD,70,Goalkeeper_Color 
    
     
+    
+    
+
 ;Wait for key from keyboard to shoot
     
- CHECK:
-;Get Curve co
+ CHECK: 
+    mov ah, 2h              ;Setting the Status Bar
+    mov bh, 0      
+    mov dl, 0 
+    mov dh, 14
+    int 10h
+     
+    mov ah,9                ;Clear The Status Bar
+    mov bh,0 
+    mov al,32               ;Printing Space
+    mov cx,80               ;80 Times to cover all Horizontal Line
+    mov bl ,0AFh            ;Set Status Bar Color -> white on Green Background
+    int 10h
+    
+    cmp current_player,1
+    JE p1
+    
+    jmp p2
+  
+        p1: 
+          mov ah, 2h
+          mov bh, 0      
+          mov dl, 0 
+          mov dh, 14
+          int 10h
+          
+          mov ah, 9h
+		  mov dx, offset Turn1
+		  int 21h
+		  
+          jmp Cont
+    
+        p2: 
+          mov ah, 2h
+          mov bh, 0      
+          mov dl, 0 
+          mov dh, 14
+          int 10h
+          
+          mov ah, 9h
+          mov dx, offset Turn2
+          int 21h
+		  
+          jmp Cont  
+    
+    Cont:
+                            ;Get Curve co
         mov bl,0 
-        mov cx,Coordinate_BallCurve[2] ; radius
+        mov cx,Coordinate_BallCurve[2] ; radius 
+          
+        
         
     FirstHalfCycle:  
                       
@@ -79,8 +146,8 @@ MAIN    PROC
        push dx
        
        ;Draw 'o'
-		Call DrawBall
-		
+       Call DrawBall 
+       
        Call Delay
        
        pop dx
@@ -99,7 +166,7 @@ MAIN    PROC
        MOV AH,1              ;Get key pressed
        INT 16H  
    
-       JNZ Key_Pressed1
+       JNE Key_Pressed1
        JMP NO_Key_Pressed1 
        
 CHECK_1:
@@ -110,8 +177,9 @@ JMP CHECK
        INT 16H  
    
        cmp ah,Shoot_Key   
-       JZ Shoot     
-   
+       JE Shoot     
+       cmp ah,1H                  ;Esc
+       JE Exit_1
        push ax
        push bx
        push cx
@@ -145,8 +213,8 @@ JMP CHECK
        push dx
        
        ;Draw 'o'
-	   Call DrawBall
-		
+       Call DrawBall 
+       
        Call Delay
        
        pop dx
@@ -167,15 +235,16 @@ JMP CHECK
         MOV AH,1              ;Get key pressed
         INT 16H  
         
-        JNZ Key_Pressed2
+        JNE Key_Pressed2
         JMP NO_Key_Pressed2               
         
     Key_Pressed2:   
         MOV AH,0             ;clear used scan code from buffer
         INT 16H  
         cmp ah,Shoot_Key   
-        JZ Shoot     
-        
+        JE Shoot     
+        cmp ah,1H                  ;Esc
+        JE Exit
         push ax
         push bx
         push cx
@@ -194,8 +263,10 @@ JMP CHECK
                    
 CMP ah,Shoot_Key 
 
-JNZ CHECK_1   
+JNE CHECK_1   
 
+Exit_1:
+JMP Exit
 
 ;si contains co-ordinate of start of shooting line
 
@@ -209,7 +280,7 @@ Shoot:
         MOV AH,1              ;Get key pressed
         INT 16H  
        
-        JNZ Key_Pressed3
+        JNE Key_Pressed3
         JMP NO_Key_Pressed3               
        
     Key_Pressed3:   
@@ -236,8 +307,8 @@ Shoot:
         
         push dx
         
-        ;Draw '0'
-		Call DrawBall
+        ;Draw
+        Call DrawBall
         
         Call Delay
         
@@ -254,37 +325,50 @@ Shoot:
         mov dl,' '
         int 21h  
      
-        add bl ,6    
-             
-                       
-    Loop Horizontal
-
-   
-JMP CHECK_1 ;Remove this line later
-
+        add bl ,6 
+        
+        ;Fady's Part
+		mov ah,3h
+		mov bh,0h
+		int 10h
+         
+		cmp dl,69                             ;X is higher than the Goal Line
+		JAE GoToCheckScores  
+		
+    Loop Horizontal         
+	
+    GoToCheckScores:
+		Call CheckScores
+    jmp CHECK_1 
+        
     
-   mov ah, 4ch
-   int 21h
+Exit: 
+    mov ah,004CH
+    int 21H    
+
+
 MAIN        ENDP    
 
 Move PROC
    RightU:
     CMP Ah,48h
-        JNZ RightD    
+    JNE RightD    
     Call RightUp
    RightD:    
     CMP Ah,50h
-    JNZ ENDD       
+    JNE ENDD       
     CALL RightDown
    ENDD:ret
 Move ENDP    
 
 RightUp PROC
    
-     cmp RBCenterU,0
-      jz ENDRU
+    cmp RBCenterU,0
+    JE ENDRU
+     
     dec RBCenterU
     print RBCenterU,70,Goalkeeper_Color
+    Delete RBCenterD,70
     Delete RBCenterD,70 
     DEC RBCenterD
    
@@ -292,8 +376,8 @@ RightUp PROC
 RightUp ENDP
 
 RightDown PROC
-    cmp RBCenterD,14
-      jz ENDRD
+    cmp RBCenterD,13
+    JE ENDRD
     
     inc RBCenterD
     print RBCenterD,70,Goalkeeper_Color 
@@ -408,42 +492,18 @@ DrawInterface   PROC
     int 10h
                             
     cmp current_player, 1  ;Check if the current player is player1
-    jz write_1
+    JE write_1
     
     ;if player 2 => Print 2
-	mov al, 0
-    mov ah, 2
-    mov dl, 6 ;Move to position X=6
-    mov dh, 8 ;Move to position Y=8
-    int 10h
-	
-    mov bh, 0
-    mov dx, 0
-	mov al, '2'
-	mov bl, Player_Color
-    mov ah, 9
-    mov cx, 1
-    int 10h
+	Call Print_P2
     
     mov ax,0
     AND ax,ax
-    jz continue1
+    JE continue1
     
+	write_1:
     ;if player 1 => Print 1
-    write_1:
-	mov al, 0
-    mov ah, 2
-    mov dl, 6 ;Move to position X=6
-    mov dh, 8 ;Move to position Y=8
-    int 10h
-	
-    mov bh, 0
-    mov dx, 0
-	mov al, '1'
-	mov bl, Player_Color
-    mov ah, 9
-    mov cx, 1
-    int 10h
+    Call Print_P1
     
     continue1:
     
@@ -463,7 +523,7 @@ DrawInterface   PROC
         mov bl, GoalDim[2]
         inc bl
         cmp cl, bl 
-        jnz loop0 
+        JNE loop0 
         
         
         ;Draw the back of the goal (|)
@@ -475,7 +535,7 @@ DrawInterface   PROC
             pop cx
             inc cl
             cmp cl, GoalDim[3]
-        jnz loop1
+        JNE loop1
         
         
         ;Draw colored spaces indicating the goal ending
@@ -490,7 +550,7 @@ DrawInterface   PROC
         mov bl, GoalDim[0]
         dec bl
         cmp cl, bl        
-        jnz loop2 
+        JNE loop2 
         
     pop ds
     pop dx
@@ -523,7 +583,7 @@ mov dx, 0
         pop cx      
         inc cl
         cmp cl, 80
-    jnz loop3
+    JNE loop3
     
     ;Move to the next line
     
@@ -566,7 +626,7 @@ mov dx, 0
     ;Move to write player 2 info
     mov ah, 2      
     mov dh, 16 ;Move to position Y=16 
-    mov dl, 78 
+    mov dl, 73 
     sub dl, p2_name[1]
     int 10h
     
@@ -606,7 +666,7 @@ mov dx, 0
         pop cx
         inc cl
         cmp cl, 80
-    jnz loop6
+    JNE loop6
     
     pop ds
     pop dx
@@ -626,7 +686,7 @@ Delay  PROC
     push dx
     push ds
     
-    ;50 ms delay        
+    ;80 ms delay        
     mov cx, 1h
     mov dx, 3880h
     mov ah, 86h
@@ -663,5 +723,322 @@ DrawBall PROC
 
 	RET
 DrawBall ENDP
+
+Print_P1 PROC
+	push ax
+	push bx
+	push cx
+	push dx
+	
+	mov al, 0
+    mov ah, 2
+    mov dl, 6 ;Move to position X=6
+    mov dh, 8 ;Move to position Y=8
+    int 10h
+	
+    mov bh, 0
+    mov dx, 0
+	mov al, '2'
+	mov bl, Player_Color
+    mov ah, 9
+    mov cx, 1
+    int 10h
+	
+	pop dx
+	pop cx
+	pop bx
+	pop ax
+	
+	RET
+Print_P1 ENDP
+
+Print_P2 PROC
+	push ax
+	push bx
+	push cx
+	push dx
+	
+	mov al, 0
+    mov ah, 2
+    mov dl, 6 ;Move to position X=6
+    mov dh, 8 ;Move to position Y=8
+    int 10h
+	
+    mov bh, 0
+    mov dx, 0
+	mov al, '2'
+	mov bl, Player_Color
+    mov ah, 9
+    mov cx, 1
+    int 10h
+	
+	pop dx
+	pop cx
+	pop bx
+	pop ax
+	
+	RET
+Print_P2 ENDP
+
+ChangeScore PROC   ;taken from write in fifth of screen
+    
+	push ax
+	push bx
+	push cx
+	push dx
+	
+    mov ah, 2
+    mov bh,0      
+    mov dl, 0 ;Move to position X=0
+    mov dh, 16 ;Move to position Y=21
+    int 10h 
+    
+    
+    ;Writing players' names and scores
+    
+    ;Player 1 Info  
+    
+    push bx
+    push cx
+    mov cx, 0 
+    mov bx, 2
+    mov cl, p1_name[1]
+    p1loop:
+    mov ah,2
+    mov dl, p1_name[bx]
+    int 21h
+    inc bx
+    loop p1loop
+    pop cx
+    pop bx
+       
+    mov ah, 2
+    mov bx, 0
+    mov dl, ':'
+    int 21h
+    
+    
+    mov bl, p1_score
+    add bl, '0'
+    mov ah, 2
+    mov dl, bl
+    int 21h
+    
+    ;Move to write player 2 info
+    mov ah, 2
+    mov bh,0      
+    mov dh, 16 ;Move to position Y=16 
+    mov dl, 73 
+    sub dl, p1_name[1]
+    int 10h
+    
+    ;Player 2 Info
+   
+    push bx
+    push cx 
+    mov bx, 2
+    mov cx, 0
+    mov cl, p2_name[1]
+    p2loop:
+    mov ah,2
+    mov dl, p2_name[bx] 
+    int 21h
+    inc bx
+    loop p2loop
+    pop cx
+    pop bx
+    
+    mov ah, 2
+    mov dl, ':'
+    int 21h
+           
+    mov bl, p2_score
+    add bl, '0'
+    mov ah, 2
+    mov dl, bl
+    int 21h  
+    
+	pop dx
+	pop cx
+	pop bx
+	pop ax
+	
+    RET
+ChangeScore ENDP 
+
+CheckScores PROC
+	push ax
+	push bx
+	push cx
+	push dx
+	
+	Passed: 
+        ;Check if it is saved
+        cmp dh,RBCenterU
+        JE Saved
+        
+        cmp dh,RBCenterD
+        JE Saved
+        
+    
+        ;Between the Goal's Region 
+        cmp dh,3
+        JBE Outline
+        cmp dh,11
+        JAE Outline
+        
+        JMP Goal
+        
+    Outline:
+        mov ah, 2h
+        mov bh, 0      
+        mov dl, 0 
+        mov dh, 14
+        int 10h
+          
+        mov ah, 9h
+        mov dx, offset Outside
+        int 21h
+        JMP Switch   
+        
+    Saved: 
+        mov ah, 2h
+        mov bh, 0      
+        mov dl, 0 
+        mov dh, 14
+        int 10h
+          
+        mov ah, 9h
+        mov dx, offset Save
+        int 21h
+        JMP Switch
+          
+    
+    Goal:
+      cmp current_player,1
+      JE incp1
+      
+      cmp current_player,2 
+      JE incp2
+         
+                                          ;Incrementing The Scores
+    incp1:
+      mov ah, 2h
+      mov bh, 0      
+      mov dl, 0 
+      mov dh, 14
+      int 10h
+          
+      mov ah, 9h
+	  mov dx, offset Goal1
+	  int 21h
+      inc p1_score      
+      jmp Switch  
+      
+    incp2:
+      mov ah, 2h
+      mov bh, 0      
+      mov dl, 0 
+      mov dh, 14
+      int 10h
+          
+      mov ah, 9h
+	  mov dx, offset Goal2
+	  int 21h
+      inc p2_score      
+      jmp Switch 
+      
+      
+      
+    Switch:
+       ;Switch the players
+       cmp current_player ,1
+       JE Setp1
+                            
+       cmp current_player ,2                             
+       JE Setp2
+       
+     
+    Setp1:
+          mov current_player ,2 
+          
+          jmp Final
+    
+    Setp2:
+          mov current_player ,1 
+
+          jmp Final  
+    
+      
+   Final:   
+      call ChangeScore  
+      
+      cmp Total_Shoots,10
+      JE Result
+      
+      inc Total_Shoots
+      
+      jmp Exit_CheckScores         
+                                          
+    
+    Result:
+        mov ah, 0
+        mov al, 3h
+        int 10h 
+    
+        mov dh,p1_score
+        mov dl,p2_score
+        cmp dl,dh
+        JB P1Win
+        
+        cmp dl,dh
+        JA P2Win
+        
+        
+        JMP Draws
+    
+    
+    P1Win:
+        mov ah,2
+        mov dh,12
+        mov dl,32
+        int 10h
+    
+        mov ah, 9
+        mov dx, offset p1_win
+        int 21h
+        jmp Exit_CheckScores
+        
+    P2Win:
+        mov ah,2
+        mov dh,12
+        mov dl,32
+        int 10h
+    
+        mov ah, 9
+        mov dx, offset p2_win
+        int 21h 
+        jmp Exit_CheckScores         
+        
+     Draws:
+        mov ah,2
+        mov dh,12
+        mov dl,32
+        int 10h
+    
+        mov ah, 9
+        mov dx, offset Draw
+        int 21h
+
+
+	Exit_CheckScores:
+	pop dx
+	pop cx
+	pop bx
+	pop ax
+	
+	RET
+CheckScores ENDP
+
 
 END MAIN                   	
